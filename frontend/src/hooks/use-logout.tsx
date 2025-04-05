0d9d82de3741f4f6ec8f02b09a771762ef86c8a2
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2025-04-05 20:00:13 -0300

    feat(session): add logout session

---
+++ frontend/src/hooks/use-logout.tsx
@@ -0,0 +1,19 @@
+import { useSessionStore } from "@/store/use-session-store";
+import Cookies from "js-cookie";
+import { useState } from "react";
+
+export const useLogout = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { removeSession } = useSessionStore();
+
+  const exec = () => {
+    setIsLoading(true);
+
+    Cookies.remove("session");
+    removeSession();
+
+    setIsLoading(false);
+  };
+
+  return { exec, isLoading };
+};
---
+++ frontend/src/store/use-session-store.tsx
@@ -0,0 +1,14 @@
+import { IAuthSession } from "@/types/auth";
+import { create } from "zustand";
+
+type TStore = {
+  session: IAuthSession | undefined;
+  setSession: (sessin: IAuthSession) => void;
+  removeSession: () => void;
+};
+
+export const useSessionStore = create<TStore>((set) => ({
+  session: undefined,
+  setSession: (session) => set({ session }),
+  removeSession: () => set(() => ({ session: undefined })),
+}));
