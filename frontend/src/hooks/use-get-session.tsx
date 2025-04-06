163a33b2afb7c8e7e2e29cf954ab70b49a1620c9
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2025-04-06 11:46:17 -0300

    fiz(auth): integrate with zustand session

--- frontend/src/hooks/use-get-session.tsx
+++ frontend/src/hooks/use-get-session.tsx
@@ -2,22 +2,36 @@ import { useValidateToken } from "@/queries/use-validate-token";
 import { useSessionStore } from "@/store/use-session-store";
 import Cookies from "js-cookie";
 import { useEffect, useState, useCallback } from "react";
+import { IAuthSession } from "@/types/auth";
 
 export const useGetSession = () => {
-  const { removeSession } = useSessionStore();
+  const { setSession, removeSession } = useSessionStore();
   const { mutateAsync } = useValidateToken();
 
   const [isLoading, setIsLoading] = useState(true);
-  const [isValid, setIsValid] = useState<boolean | null>(null);
-  const [token, setToken] = useState<string>("");
+  const [isValid, setIsValid] = useState<boolean | undefined>(undefined);
+  const [session, setLocalSession] = useState<IAuthSession | undefined>(
+    undefined
+  );
 
-  const getToken = () => Cookies.get("session") || "";
+  const getSessionFromCookie = (): IAuthSession | undefined => {
+    const session = Cookies.get("session");
+    try {
+      const parsed = JSON.parse(session || "{}");
+      if (parsed.token && parsed.id) {
+        return parsed as IAuthSession;
+      }
+      return undefined;
+    } catch (e) {
+      return undefined;
+    }
+  };
 
   const retrieveSession = useCallback(async () => {
-    const currentToken = getToken();
-    setToken(currentToken);
+    const currentSession = getSessionFromCookie();
+    setLocalSession(currentSession);
 
-    if (!currentToken) {
+    if (!currentSession?.token) {
       removeSession();
       setIsValid(false);
       setIsLoading(false);
@@ -25,18 +39,25 @@ export const useGetSession = () => {
     }
 
     try {
-      const isValidResult = await mutateAsync({ token: currentToken });
+      const isValidResult = await mutateAsync({ token: currentSession.token });
       setIsValid(isValidResult);
 
-      if (!isValidResult) removeSession();
+      if (!isValidResult) {
+        removeSession();
+        setLocalSession(undefined);
+        return;
+      }
+
+      setSession(currentSession);
     } catch (error) {
       console.error("Sessão inválida!", error);
       removeSession();
       setIsValid(false);
+      setLocalSession(undefined);
     } finally {
       setIsLoading(false);
     }
-  }, [mutateAsync, removeSession]);
+  }, [mutateAsync, removeSession, setSession]);
 
   useEffect(() => {
     retrieveSession();
@@ -45,7 +66,7 @@ export const useGetSession = () => {
   return {
     isLoading,
     isValid,
-    token,
+    session,
     revalidate: retrieveSession,
   };
 };
--- frontend/src/queries/use-login.tsx
+++ frontend/src/queries/use-login.tsx
@@ -1,8 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginService } from "@/services/auth";
+import { useSessionStore } from "@/store/use-session-store";
+import { IAuthSession } from "@/types/auth";
+import Cookies from "js-cookie";
 
 export const useLogin = () => {
+  const { setSession } = useSessionStore();
+
   return useMutation({
     mutationFn: loginService,
+    onSuccess: (data: IAuthSession) => {
+      setSession({ ...data });
+      Cookies.set("session", JSON.stringify(data));
+    },
   });
 };
